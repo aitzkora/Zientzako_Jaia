d1c10c9b0b29e8e0c4042c7558c83828976aba0a
aitzkora <marc.fuentes@inria.fr>
2013-09-16 17:02:32 +0200

    ajout description tp cesar

--- cesar.py
+++ cesar.py
@@ -1,24 +1,52 @@
-from operator import add as colle
+from string import ascii_lowercase 
+
+
 def cesar(clef, mot):
-  alpha= list('abcdefghijklmnopqrstuvwxyz')
-  decale = alpha[-clef:] + alpha[:-clef]
+  """
+  mot_code(clef, mot)
+  encode (ou decode avec une clef negatif) un mot selon le code cesar en utilisant
+  la clef
+
+  """
+  from operator import add as colle
+  alpha = list(ascii_lowercase)
+  decale = alpha[clef:] + alpha[:clef]
   table = {c:v for (c,v) in zip(alpha,decale) }
   return reduce(colle, map(lambda x:table[x], mot))
+
+def cesar_bourrin(clef, mot):
+   alpha =  list(ascii_lowercase)
+   decale = list(alpha)
+   for i in range(len(alpha)-clef):
+        decale[i] = alpha[i + clef]
+   for i in range(clef):
+        decale[len(alpha) -clef + i] = alpha[i]
+   table = {} 
+   for i in range(len(alpha)):
+         table.update({alpha[i]:decale[i]})
+   code = ''
+   for i in range(len(mot)):
+          code += table[mot[i]]
+   return code
 
 def vraisemblance(dico, phrase, clef):
     s = 0
     for mot in phrase:
-        if cesar(clef,mot) in dico:
+        if cesar(-clef,mot) in dico:
            s += 1
     return s 
+
+
+
 from numpy import loadtxt
 dico = loadtxt('dictionnaire',dtype=str).tolist()
-phrase = ['ohv', 'pdwkv', 'f','hvw','elhq']
+
+message_code = 'ohv pdwkv f hvw elhq'
+phrase = message_code.split()
 
 for clef in range(16):
     if vraisemblance(dico, phrase, clef) > 0:
         s = ''
         for mot in phrase:
-            s += ' '+ cesar(clef,mot)
-        print s    
-            
+            s += cesar(-clef,mot)+ ' '
+        print 'pour la clef %d , le message peut etre : %s' % (clef, s)    
